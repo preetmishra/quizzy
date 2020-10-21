d26654c134b74a37fc16120de8131c69675223c1
Preet Mishra <ipreetmishra@gmail.com>
2020-10-21 20:34:47 +0530

    api: Extend AbstractUser to add identifiers for students and teachers.

--- api/models.py
+++ api/models.py
@@ -1,1 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    """
+    A subclass to AbstractUser class for extra fields.
+    """
+
+    is_teacher = models.BooleanField(
+        default=False,
+        verbose_name='Teacher status',
+        help_text='Designates whether this user should be treated as a '
+                  'teacher.',
+    )
+    is_student = models.BooleanField(
+        default=False,
+        verbose_name='Student status',
+        help_text='Designates whether this user should be treated as a '
+                  'student.',
+    )
