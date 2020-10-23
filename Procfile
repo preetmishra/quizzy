66b012f2ae92464c25d436aaafb55bdc50c53b1d
Preet Mishra <ipreetmishra@gmail.com>
2020-10-23 10:24:19 +0530

    deploy: Deploy app to heroku.

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate --no-input
+web: gunicorn quizzy.wsgi --log-file -
--- quizzy/settings.py
+++ quizzy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,5 @@ AUTH_USER_MODEL = 'api.User'
 
 CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['*']
+
+django_heroku.settings(locals())
